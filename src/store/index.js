c768c795a34dc93359fa950076f5452c411351aa
Stefan <stefan.deak@computerrock.com>
2020-07-02 18:49:48 +0200

    Clear console log

--- src/store/index.js
+++ src/store/index.js
@@ -11,27 +11,3 @@ const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger));
 sagaMiddleware.run(rootSaga);
 
 export { store as default };
-
-
-// import createSagaMiddleware from 'redux-saga';
-// import {applyMiddleware, createStore} from 'redux';
-// import logger from 'redux-logger';
-// import rootReducer from '../reducers';
-// import rootSaga from '../sagas';
-// import AuthService from '../services/AuthService';
-// import {DictionaryService} from '../services/DictionaryService';
-
-// const sagaMiddleware = createSagaMiddleware();
-
-// const store = createStore(
-//     rootReducer,
-//     {
-//         UserAuthReducer: AuthService.getUserData(),
-//         LocalisationReducer: DictionaryService.getLocaleData(),
-//     },
-//     applyMiddleware(sagaMiddleware, logger)
-// );
-
-// sagaMiddleware.run(rootSaga);
-
-// export {store as default};
